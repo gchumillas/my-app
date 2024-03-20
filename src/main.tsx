93df792382121d455061aa9e3f000e60726b2e6d
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2024-03-20 09:45:13 +0000

    remove @fraku-ui

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { GlobalStyles } from 'twin.macro'
-import '@fraku/ui/dist/fraku-ui.css'
 import store from './store'
 import App from './App.tsx'
 import './index.css'
