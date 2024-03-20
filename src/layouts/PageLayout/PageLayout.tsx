9274cabe7fea70c6777690ef782cbbd52e378945
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2024-03-20 09:43:56 +0000

    update page layout

--- src/layouts/PageLayout/PageLayout.tsx
+++ src/layouts/PageLayout/PageLayout.tsx
@@ -1,7 +1,7 @@
 import { PageLayoutProps } from './types'
 
 export const PageLayout = ({ children }: PageLayoutProps) => {
-  return <div className="w-full max-w-6xl mx-auto py-8">{children}</div>
+  return <div className="w-full max-w-5xl mx-auto py-8">{children}</div>
 }
 
 export default PageLayout
